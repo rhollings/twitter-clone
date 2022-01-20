6e5d5e0605bbecbc598fc3a6513f686906442623
Rod <rhollingsworth1997@gmail.com>
2022-01-20 13:31:41 +0100

    Uploading

---
+++ src/TweetBox.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import "./TweetBox.css";
+import { Avatar, Button } from "@material-ui/core";
+import db from "./firebase";
+
+function TweetBox() {
+  const [tweetMessage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState("");
+
+  const sendTweet = (e) => {
+    e.preventDefault();
+
+    db.collection("posts").add({
+      displayName: "Richard Grayson",
+      username: "notnightwing",
+      verified: true,
+      text: tweetMessage,
+      image: tweetImage,
+      avatar:
+        "https://photos1.iorbix.com/00/00/00/00/01/88/86/10/Richard-Grayson-kmh4G2kjZ-b.jpg",
+    });
+
+    setTweetMessage("");
+    setTweetImage("");
+  };
+
+  return (
+    <div className="tweetBox">
+      <form>
+        <div className="tweetBox__input">
+          <Avatar src="https://i.ytimg.com/vi/Y8imNSyccwg/maxresdefault.jpg" />
+          <input
+            onChange={(e) => setTweetMessage(e.target.value)}
+            value={tweetMessage}
+            placeholder="What's happening?"
+            type="text"
+          />
+        </div>
+        <input
+          value={tweetImage}
+          onChange={(e) => setTweetImage(e.target.value)}
+          className="tweetBox__imageInput"
+          placeholder="Optional: Enter image URL"
+          type="text"
+        />
+
+        <Button
+          onClick={sendTweet}
+          type="submit"
+          className="tweetBox__tweetButton"
+        >
+          Tweet
+        </Button>
+      </form>
+    </div>
+  );
+}
+
+export default TweetBox;
---
+++ src/Widgets.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Widgets.css";
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
+import SearchIcon from "@material-ui/icons/Search";
+
+function Widgets() {
+  return (
+    <div className="widgets">
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+
+      <div className="widgets__widgetContainer">
+        <h2>What's happening</h2>
+
+        <TwitterTweetEmbed tweetId={"1483427664518164485"} /> 
+
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="elonmusk"
+          options={{ height: 400 }}
+        />
+
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="NuclearRodBomb"
+          options={{ height: 400 }}
+        />
+
+        <TwitterShareButton
+          url={"https://create-react-app.dev/"}
+          options={{ text: "#reactjs to the moon", via: "reactjs" }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Widgets;
